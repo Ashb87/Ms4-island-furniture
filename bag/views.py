8af7ecd8918cd2f937385095de6148bd841f37bf
Ashb87 <ashleyb182@hotmail.co.uk>
2022-01-07 20:20:27 +0000

    added toasts

--- bag/views.py
+++ bag/views.py
@@ -3,12 +3,12 @@ from django.contrib import messages
 
 from products.models import Product
 
+# Create your views here.
 
 def view_bag(request):
     """ A view that renders the bag contents page """
 
     return render(request, 'bag/bag.html')
-
 
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
@@ -88,3 +88,5 @@ def remove_from_bag(request, item_id):
     except Exception as e:
         messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
+
+
