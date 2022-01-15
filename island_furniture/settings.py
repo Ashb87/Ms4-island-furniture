81957dc81bf93e04fee8eb464ad2367d8aa4ad11
Ashb87 <ashleyb182@hotmail.co.uk>
2022-01-15 18:35:49 +0000

    deployment to heroku part 2

--- island_furniture/settings.py
+++ island_furniture/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-8-tb@-a9p==$(05!=wu@_4$*l*@uh(p_v)-*h=y^(g1vkxc-$n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ms4-island-furniture.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -119,12 +119,17 @@ WSGI_APPLICATION = 'island_furniture.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
