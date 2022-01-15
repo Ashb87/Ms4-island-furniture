1de862104ca1eea2b41205709aa29526cea73a59
Ashb87 <ashleyb182@hotmail.co.uk>
2022-01-15 18:52:05 +0000

    removed secret key and set debug

--- island_furniture/settings.py
+++ island_furniture/settings.py
@@ -19,10 +19,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8-tb@-a9p==$(05!=wu@_4$*l*@uh(p_v)-*h=y^(g1vkxc-$n'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
+
 
 ALLOWED_HOSTS = ['ms4-island-furniture.herokuapp.com', 'localhost']
 
