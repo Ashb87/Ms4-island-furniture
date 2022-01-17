6ad4308b2cf7e9bda1f7570a9899469f8a2c444c
Ashb87 <ashleyb182@hotmail.co.uk>
2022-01-17 19:57:53 +0000

    changed hero image and fixed problems in python files

--- products/views.py
+++ products/views.py
@@ -4,8 +4,8 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.db.models.functions import Lower
 
-from .models import Product, Category, ProductReview #
-from .forms import ReviewForm, ProductForm #
+from .models import Product, Category, ProductReview  #
+from .forms import ReviewForm, ProductForm  #
 
 
 def all_products(request):
@@ -68,13 +68,13 @@ def product_detail(request, product_id):
 
     context = {
         'product': product,
-        'form': form, #
+        'form': form,  #
     }
 
     return render(request, 'products/product_detail.html', context)
 
 
-@login_required #
+@login_required  #
 def add_review(request, product_id):
     """ Allow user to add review """
 
@@ -100,7 +100,7 @@ def add_review(request, product_id):
     return render(request, context)
 
 
-@login_required #
+@login_required  #
 def edit_review(request, review_id):
     """ Allow user to edit their review """
 
@@ -137,7 +137,7 @@ def add_product(request):
     if not request.user.is_superuser:
         messages.error(request, 'Sorry, only store owners can do that.')
         return redirect(reverse('home'))
-        
+
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
@@ -145,10 +145,11 @@ def add_product(request):
             messages.success(request, 'Successfully added product!')
             return redirect(reverse('product_detail', args=[product.id]))
         else:
-            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+            messages.error(request, 'Failed to add product. \
+                Please ensure the form is valid.')
     else:
         form = ProductForm()
-        
+
     template = 'products/add_product.html'
     context = {
         'form': form,
@@ -172,7 +173,8 @@ def edit_product(request, product_id):
             messages.success(request, 'Successfully updated product!')
             return redirect(reverse('product_detail', args=[product.id]))
         else:
-            messages.error(request, 'Failed to update product. Please ensure the form is valid.')
+            messages.error(request, 'Failed to update product. \
+                Please ensure the form is valid.')
     else:
         form = ProductForm(instance=product)
         messages.info(request, f'You are editing {product.name}')
