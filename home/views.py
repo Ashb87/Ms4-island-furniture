3b7380370d41bc626bfabfee3a2538e37bf4c411
Ashb87 <ashleyb182@hotmail.co.uk>
2022-02-04 14:18:37 +0000

    added 404 error handler

--- home/views.py
+++ home/views.py
@@ -7,3 +7,6 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+def entry_not_found(request, exception, template_name='404.html'):
+    return render(request, '404.html')
