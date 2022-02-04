462e7a6127f1f65df61fce6f849233873af31e48
Ashb87 <ashleyb182@hotmail.co.uk>
2022-02-04 23:42:19 +0000

    deleted error handler code after finding issue that was stopping error pages from loading

--- island_furniture/urls.py
+++ island_furniture/urls.py
@@ -27,5 +27,3 @@ urlpatterns = [
     path('profile/', include('profiles.urls')),
     path('contact/', include('contact.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-handler404 = 'home.views.entry_not_found'
