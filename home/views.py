60bc9a57064bde70b940fc2f28cab1c87b680cea
Ashb87 <ashleyb182@hotmail.co.uk>
2022-02-04 11:29:03 +0000

    deleted code trying to fix problem with error pages

--- home/views.py
+++ home/views.py
@@ -6,7 +6,3 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
-
-def entry_not_found(request, exception, template_name='404.html'):
-    """ displays 404 template """
-    return render(request, '404.html')
