e10c7c821c2ba1e7213d371b81921d3049693ac9
Ashb87 <ashleyb182@hotmail.co.uk>
2022-02-05 09:30:12 +0000

    set debug

--- island_furniture/settings.py
+++ island_furniture/settings.py
@@ -25,6 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
+
 ALLOWED_HOSTS = ['ms4-island-furniture.herokuapp.com', 'localhost']
 
 
