462c944d7288db7d1c39a1373e6a4781ffac85ff
Ashb87 <ashleyb182@hotmail.co.uk>
2022-02-06 01:12:38 +0000

    edited placeholder text

--- profiles/forms.py
+++ profiles/forms.py
@@ -17,9 +17,9 @@ class UserProfileForm(forms.ModelForm):
         """
         super().__init__(*args, **kwargs)
         placeholders = {
-            'default_first_name': 'Name',
+            'default_first_name': 'First Name',
             'default_surname': 'Surname',
-            'default_email': 'email address',
+            'default_email': 'Email Address',
             'default_phone_number': 'Phone Number',
             'default_postcode': 'Postal Code',
             'default_town_or_city': 'Town or City',
